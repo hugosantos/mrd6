4b4ec820965d21b521a30cf2b483814b833f1edd
Hugo Santos <hugo@fivebits.net>
2007-01-07 02:14:32 +0000

    updated copyright info.

--- include/mrd/support/ptree.h
+++ include/mrd/support/ptree.h
@@ -1,4 +1,28 @@
 /*
+ * Multicast Routing Daemon (MRD)
+ *   ptree.h
+ *
+ * Copyright (C) 2006, 2007 - Hugo Santos
+ * Copyright (C) 2004..2006 - Universidade de Aveiro, IT Aveiro
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * Author:  Hugo Santos <hugo@fivebits.net>
+ */
+
+/*
  * Patricia tree implementation, compressed binary trie
  *
  *   based on
@@ -6,10 +30,6 @@
  * PATRICIA - Pratical Algorithm to Retrieve Information Coded
  *            in Alphanumeric - Donald R. Morrison 1968
  *
- * Copyright (C) 2004, 2005, 2006
- *   Universidade de Aveiro, Instituto de Telecomunicacoes - Polo Aveiro
- *
- * Author: Hugo Santos <hsantos@av.it.pt>
  */
 
 #ifndef _Ptree_h_
