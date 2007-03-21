355142913f514b6d67bd70617bb6ca4caecea138
Hugo Santos <hugo@fivebits.net>
2007-03-21 02:09:39 +0000

    * check if mrib works properly when the entries are installed in reverse order

--- src/tests/mrib_unittest.cpp
+++ src/tests/mrib_unittest.cpp
@@ -13,15 +13,36 @@ class test_origin : public mrib_origin {
 	}
 };
 
-#define ADDR(str)	inet6_addr(string(str))
-#define ANY		inet6_addr::any()
+static inet6_addr ADDR(const char *str) {
+	return inet6_addr(string(str));
+}
 
-static void prepare_prefix(mrib_def::prefix *p) {
-	p->nexthop = ADDR("2001::1");
+static inet6_addr ANY() {
+	return inet6_addr::any();
+}
+
+static mrib_def::prefix *new_prefix(mrib_origin *o) {
+	mrib_def::prefix *p = new mrib_def::prefix(o);
+	p->nexthop = inet6_addr::any();
 	p->distance = 0;
 	p->metric = 0;
 	p->flags = 0;
 	p->intf = NULL;
+	return p;
+}
+
+static void test1(mrib_def &m, test_origin &o,
+		  const inet6_addr &pfx1, mrib_def::prefix *p1,
+		  const inet6_addr &pfx2, mrib_def::prefix *p2) {
+	BOOST_CHECK(m.get_prefix(pfx1, NULL) == p1);
+	BOOST_CHECK(m.get_prefix(pfx1, &o) == p1);
+
+	BOOST_CHECK(m.get_prefix(pfx2, NULL) == p2);
+	BOOST_CHECK(m.get_prefix(pfx2, &o) == p2);
+
+	BOOST_CHECK(m.prefix_lookup(ADDR("2001:124::1"), ANY()) == p1);
+	BOOST_CHECK(m.prefix_lookup(ADDR("2001:123::1"), ANY()) == p2);
+	BOOST_CHECK(m.prefix_lookup(ADDR("3000:"), ANY()) == NULL);
 }
 
 BOOST_AUTO_UNIT_TEST(mrib_test1) {
@@ -30,11 +51,8 @@ BOOST_AUTO_UNIT_TEST(mrib_test1) {
 
 	BOOST_REQUIRE(m.check_startup());
 
-	mrib_def::prefix *p1 = new mrib_def::prefix(&o);
-	prepare_prefix(p1);
-
-	mrib_def::prefix *p2 = new mrib_def::prefix(&o);
-	prepare_prefix(p2);
+	mrib_def::prefix *p1 = new_prefix(&o);
+	mrib_def::prefix *p2 = new_prefix(&o);
 
 	inet6_addr pfx1(ADDR("2000::/3"));
 	inet6_addr pfx2(ADDR("2000:123::/32"));
@@ -42,15 +60,27 @@ BOOST_AUTO_UNIT_TEST(mrib_test1) {
 	BOOST_REQUIRE(m.install_prefix(pfx1, p1));
 	BOOST_REQUIRE(m.install_prefix(pfx2, p2));
 
-	BOOST_CHECK(m.get_prefix(pfx1, NULL) == p1);
-	BOOST_CHECK(m.get_prefix(pfx1, &o) == p1);
+	test1(m, o, pfx1, p1, pfx2, p2);
 
-	BOOST_CHECK(m.get_prefix(pfx2, NULL) == p2);
-	BOOST_CHECK(m.get_prefix(pfx2, &o) == p2);
+	m.shutdown();
+}
 
-	BOOST_CHECK(m.prefix_lookup(ADDR("2001:124::1"), ANY) == p1);
-	BOOST_CHECK(m.prefix_lookup(ADDR("2001:123::1"), ANY) == p2);
-	BOOST_CHECK(m.prefix_lookup(ADDR("3000:"), ANY) == NULL);
+BOOST_AUTO_UNIT_TEST(mrib_test1_rev) {
+	mrib_def m(NULL);
+	test_origin o;
+
+	BOOST_REQUIRE(m.check_startup());
+
+	mrib_def::prefix *p1 = new_prefix(&o);
+	mrib_def::prefix *p2 = new_prefix(&o);
+
+	inet6_addr pfx1(ADDR("2000::/3"));
+	inet6_addr pfx2(ADDR("2000:123::/32"));
+
+	BOOST_REQUIRE(m.install_prefix(pfx2, p2));
+	BOOST_REQUIRE(m.install_prefix(pfx1, p1));
+
+	test1(m, o, pfx1, p1, pfx2, p2);
 
 	m.shutdown();
 }
