09ec62feede257388f080167cdf8d60f7e692fb1
Hugo Santos <hugo@fivebits.net>
2007-03-21 08:58:22 +0000

    * more address unit tests

--- src/tests/address_unittest.cpp
+++ src/tests/address_unittest.cpp
@@ -8,17 +8,20 @@ BOOST_AUTO_UNIT_TEST(address_test1) {
 	inet6_addr a1(std::string("::"));
 	inet6_addr a2(std::string("2000::/3"));
 	inet6_addr a3(std::string("2001:123:456::/3"));
+	inet6_addr a4(std::string("2001::1"));
+	inet6_addr a5(std::string("2002::/16"));
 
 	BOOST_CHECK(a0 == inet6_addr::any());
-
 	BOOST_CHECK(a1.addr == in6addr_any);
 	BOOST_CHECK(a1.prefixlen == 128);
-
 	BOOST_CHECK(a2.addr.s6_addr[0] == 0x20);
 	BOOST_CHECK(a2.prefixlen == 3);
 
 	a3.apply_prefixlen();
 
 	BOOST_CHECK(a2 == a3);
+	BOOST_CHECK(a0.matches(a4));
+	BOOST_CHECK(a2.matches(a4));
+	BOOST_CHECK(!a5.matches(a4));
 }
 
