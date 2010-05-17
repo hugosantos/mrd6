a4b76225504e2293dde33389a5a1a82862ff7170
Hugo Santos <hugo@fivebits.net>
2010-05-17 21:08:50 +0200

    mrd.cpp: moved version to 0.10.0 to better reflect latest changes and fixes.

--- src/mrd.cpp
+++ src/mrd.cpp
@@ -76,7 +76,7 @@
 mrd *g_mrd = 0;
 
 extern const char *BuildDate;
-static const char *VersionInfo = "mrd6 0.9.6 ($Rev$)";
+static const char *VersionInfo = "mrd6 0.10.0";
 
 static const char *defaultconffiles[] = {
 	"mrd6.conf",
